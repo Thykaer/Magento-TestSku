e3543a3ca85c927e89e3b8d3e531588e29d7736f
Martin Sinkbæk Thomsen <mst@wexo.dk>
2023-04-27 08:15:31 +0200

    [20391-395] Added functions to get newest configs

--- Model/HeyLoyaltyConfig.php
+++ Model/HeyLoyaltyConfig.php
@@ -11,6 +11,9 @@ class HeyLoyaltyConfig implements HeyLoyaltyConfigInterface
     public const CONFIG_ENABLED = 'heyloyalty/general/enabled';
     public const CONFIG_API_KEY = 'heyloyalty/general/api_key';
     public const CONFIG_API_SECRET = 'heyloyalty/general/api_secret';
+    public const CONFIG_LIST = 'heyloyalty/general/list';
+    public const CONFIG_TRACKING_ACTIVATE = 'heyloyalty/general/tracking_activate';
+    public const CONFIG_MAPPER = 'heyloyalty/general/mapper';
 
     public function __construct(
         public ScopeConfigInterface $scopeConfig
@@ -37,6 +40,30 @@ class HeyLoyaltyConfig implements HeyLoyaltyConfigInterface
     {
         return $this->scopeConfig->getValue(
             self::CONFIG_API_SECRET,
+            ScopeInterface::SCOPE_STORE
+        ) ?? '';
+    }
+
+    public function getTrackingActivated(): string
+    {
+        return $this->scopeConfig->getValue(
+            self::CONFIG_TRACKING_ACTIVATE,
+            ScopeInterface::SCOPE_STORE
+        ) ?? '';
+    }
+
+    public function getList(): string
+    {
+        return $this->scopeConfig->getValue(
+            self::CONFIG_LIST,
+            ScopeInterface::SCOPE_STORE
+        ) ?? '';
+    }
+
+    public function getMapper(): string
+    {
+        return $this->scopeConfig->getValue(
+            self::CONFIG_MAPPER,
             ScopeInterface::SCOPE_STORE
         ) ?? '';
     }
