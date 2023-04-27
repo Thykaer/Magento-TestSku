d69db84514871e90eee6e41cc08ad378e2d86c29
Martin Sinkbæk Thomsen <mst@wexo.dk>
2023-04-27 09:54:57 +0200

    [20391-395] Cleanup

--- ViewModel/Tracking.php
+++ ViewModel/Tracking.php
@@ -11,21 +11,41 @@ class Tracking
     ) {
     }
 
+    /**
+     * Get from config if tracking is activated
+     *
+     * @return bool
+     */
     public function getIsTrackingActivated(): bool
     {
         return $this->config->getIsTrackingActivated();
     }
 
+    /**
+     * Get API key from config
+     *
+     * @return string
+     */
     public function getApiKey(): string
     {
         return $this->config->getApiKey();
     }
 
+    /**
+     * Get session time
+     *
+     * @return string
+     */
     public function getSessionTime(): string
     {
         return '';
     }
 
+    /**
+     * Get tracking id from config
+     *
+     * @return string
+     */
     public function getTrackingId(): string
     {
         return $this->config->getTrackingId();
